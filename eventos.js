05e44a72cb2c8faf2fb42ade6bce718025b48470
katherine-aleman <katherinealemanmc99@gmail.com>
2024-08-17 22:17:50 -0300

    solucion final para los eventos

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
+// Manejador de eventos para el botón
+document.querySelector('button').addEventListener('click', function (event) {
+    alert('Hola!');
+    event.stopPropagation(); // Evita que el evento click se propague al div
+});
+
+// Manejador de eventos para el div
 document.querySelector('div').addEventListener('click', function () {
     alert('Hola! Soy el div');
 });
